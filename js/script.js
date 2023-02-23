33b4d1793d423b1df512155be4b3642ca5059376
Puiman0516 <lindachan0516@outlook.com>
2023-02-23 18:48:18 +0800

    fix home page header

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 //加载头部
 $(document).ready(function () {
-    if (siteName() != 'index.html') {
-        $('header').load('../html/header.html')
-        $.getScript('../js/header.js')
-    } else {
-        $('header').load('./html/header.html')
-        $.getScript('./js/header.js')
-    }
+    $('header').load('../html/header.html')
+    $.getScript('../js/header.js')
 })
-
-function siteName() {
-    var strUrl = location.href
-    var arrUrl = strUrl.split('/')
-    var str = arrUrl[arrUrl.length - 1]
-    return str
-}
